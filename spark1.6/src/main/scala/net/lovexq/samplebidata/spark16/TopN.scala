93354cfef7bf383c80c5ec20ca4d479b1c4f03cc
陆品冬 <lupindong@oristartech.com>
2019-08-15 10:53:16 +0800

    优化配置

--- spark1.6/src/main/scala/net/lovexq/samplebidata/spark16/TopN.scala
+++ spark1.6/src/main/scala/net/lovexq/samplebidata/spark16/TopN.scala
@@ -12,29 +12,28 @@ import org.apache.spark.SparkContext
 object TopN {
 
   def main(args: Array[String]): Unit = {
-
-    val conf = AppUtil.getSparkConf("TopN Application")
-    val sc = new SparkContext(conf)
-
-    val filePath = "file:///D:/home/lovexq/cloudera/topn"
-
-    val dataRDD = sc.textFile(filePath).cache
-
-    var n = 5
-
+    // 默认数据目录
+    var filePath = "file:///D:/WorkSpaces/2019/bigdata-sample/spark1.6/src/main/resources/data/topn"
     if (!args.isEmpty) {
+      filePath = args(0)
+    }
+
+    // 默认取前5名
+    var n = 5
+    if (!args.isEmpty && args.length > 1) {
       n = args(0).toInt
     }
 
-    println(s"n is ${n}")
+    val conf = AppUtil.getSparkConf("TopN Application")
+    val sc = new SparkContext(conf)
+    val dataRDD = sc.textFile(filePath).cache
 
     dataRDD
       // 过滤出非空行，且有四列的数据
       .filter(e => e != null && !e.trim.isEmpty && e.trim.split(",").length == 4)
       // 按逗号分割每行数据，取payment的值
-      .map(_.trim.split(",")(2))
-      .map((_, 1))
-      .sortByKey(false)
+      .map(_.trim.split(",")(2).trim().toInt)
+      .sortBy(e => e, false)
       .take(n)
       .foreach(println)
 
